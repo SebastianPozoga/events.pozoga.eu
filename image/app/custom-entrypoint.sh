52c4b5d68c41a8733a031dfa6286e94b438ad0ab
Sebastian Pożoga <prog255@gmail.com>
2018-08-20 05:39:00 +0200

    add migration envs

--- image/app/custom-entrypoint.sh
+++ image/app/custom-entrypoint.sh
@@ -4,12 +4,27 @@ set -e
 sh ./wait-for-db.sh $DB_HOST $DB_PORT
 
 # Prepare wordpress
-wp config create --dbhost="$DB_HOST:$DB_PORT" --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --locale=ro_RO
+if [ ! -f "/app/wp-config.php" ]; then
+  wp config create --dbhost="$DB_HOST:$DB_PORT" --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --locale=ro_RO
+fi
+
+
 if $(wp core is-installed); then
   wp user update $WP_EMAIL --user_pass=$WP_PASS
 fi
+
 if ! $(wp core is-installed); then
   wp core install --url=$WP_HOST --title="$WP_TITLE" --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL
+fi
+
+if [ ! -z "$MIGRATE_FROM" ]; then
+  echo "Migrate database $MIGRATE_FROM to $MIGRATE_TO"
+  wp search-replace $MIGRATE_FROM $MIGRATE_TO
+  if $(wp --url=$MIGRATE_FROM core is-installed --network); then
+    wp search-replace --url=$MIGRATE_FROM "$MIGRATE_FROM" "$MIGRATE_TO" --recurse-objects --network --skip-columns=guid --skip-tables=wp_users
+  else
+    wp search-replace "$MIGRATE_FROM" "$MIGRATE_TO" --recurse-objects --skip-columns=guid --skip-tables=wp_users
+  fi
 fi
 
 # active plugins
